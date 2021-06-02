c57634ccde62db9cdd3a1e906717a3e996df8775
Agnieszka Polińska <agapoli@gmail.com>
2021-06-02 18:47:44 +0200

    Prepare errors holding

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ class App extends Component {
     email: "",
     pass: "",
     accept: false,
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    }
+  }
+
+  messages = {
+    username_incorrect: "Nazwa musi być dłuższa niż 3 znaki i nie może zawierać spacji",
+    email_incorrect: "Niepoprawny adres email, dodaj @",
+    pass_incorrect: "Hasło musi mieć 8 znaków",
+    accept_incorrect: "Niepotwierdzona zgoda",
   }
 
   handleChange = e => {
@@ -39,16 +52,20 @@ class App extends Component {
       <form className="App" onSubmit={this.handleSubmit} noValidate>
       <label htmlFor="user">Twoje imię:  
         <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}/>
+        {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
       </label>
       <label htmlFor="email">Twój adres email:  
         <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange}/>
+        {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
       </label>
       <label htmlFor="password">Twoje hasło:  
         <input type="password" id="password" name="pass" value={this.state.pass} onChange={this.handleChange}/>
+        {this.state.errors.pass && <span>{this.messages.pass_incorrect}</span>}
       </label>
       <label htmlFor="accept">
         <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange}/> Wyrażam zgodę na wszystko
       </label>
+      {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
       <button>Zapisz się</button>
     </form>
     );

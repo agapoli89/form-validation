26b704279741fb6bf7f1768a5ebd54d3dde97b33
Agnieszka Polińska <agapoli@gmail.com>
2021-06-03 07:16:37 +0200

    Validate form

--- src/App.js
+++ src/App.js
@@ -44,7 +44,68 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log('działa')
+
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        pass: "",
+        accept: false,
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        }
+      })
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pass: !validation.password,
+          accept: !validation.accept,
+        }
+      })
+    }
+  }
+ 
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (this.state.username.length > 3 && this.state.username.indexOf(' ') === -1) {
+      username = true;
+    }
+
+    if (this.state.email.indexOf('@') !== -1) {
+      email = true
+    }
+
+    if (this.state.pass.length === 8) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept) {
+      correct = true;
+    }
+
+    return({
+      correct,
+      username,
+      email,
+      password,
+      accept
+    })
   }
   
   render() { 
